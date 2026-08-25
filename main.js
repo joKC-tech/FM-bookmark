187226ff51360822dce2e38edff1d7851eea1bf6
joKC-tech <jo@karmacampaigns.com>
2026-08-26 08:56:18 +1000

    mobile view

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Burger menu
+
 const menuBtn = document.getElementById('menu-btn');
 const mainMenu = document.getElementById('main-menu');
 const menuIcon  = document.querySelector('#menu-btn img')
@@ -19,6 +21,7 @@ function handleMenuClick(){
 
 menuBtn.addEventListener('click', handleMenuClick)
 
+// Tabs
 
 const tabs = document.querySelectorAll('[role="tab"');
 const panels = document.querySelectorAll('[role="tabpanel"]')
@@ -31,3 +34,31 @@ tabs.forEach( tab => tab.addEventListener('click', ()=>{
     panels.forEach (p => p.classList.toggle( 'flex' , p.id === tab.getAttribute('aria-controls')));
 }))
 
+// Sign up form
+
+const signUp = document.getElementById('sign-up');
+const emailError = document.getElementById('email-error');
+const emailGroup = document.getElementById('email-field-group');
+const email = document.getElementById('email')
+
+function handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(signUp);
+
+    console.log(data.get('email'))
+    if(!validator.isEmail(data.get('email'))){
+        console.log('email invalid')
+        email.setAttribute('aria-invalid', 'true');
+        emailError.innerHTML = `Whoops, make sure it's an email`;
+        emailGroup.classList.add('bg-red-400');
+    } else { 
+        email.setAttribute('aria-isvalid', 'false');
+        emailError.innerHTML = `Thanks, your on our list!`;
+        emailGroup.classList.remove('bg-red-400');
+        email.value = ``;
+
+
+    }
+}
+
+signUp.addEventListener('submit' , handleSubmit)
